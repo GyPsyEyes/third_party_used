83e9bb443740396aca264e6efdb7660c2f306882
hengxin <xin.heng@oceanus-plus.com>
2019-09-30 10:20:26 +0800

    删除不用文件

--- src/main/java/com/oceanus/pay/constants/YopConst.java
+++
@@ -1,6 +0,0 @@
-package com.oceanus.pay.constants;
-
-public class YopConst {
-
-    public static final String FAILURE = "FAILURE";
-}
--- src/main/java/com/oceanus/pay/yeepay/service/YeePayService.java
+++
@@ -1,7 +0,0 @@
-package com.oceanus.pay.yeepay.service;
-
-import java.util.Map;
-
-public interface YeePayService {
-    Map<String, Object> yeePayYop(Map<String, Object> param, String url);
-}
